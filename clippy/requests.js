62253412c9052b8c9dd42b6228dfcfbc5cad3d97
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2018-11-29 12:50:57 -0600

    forgot to git add

---
+++ clippy/requests.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+export default class Requests {
+
+    static baseURL = "http://34.224.86.78:8080/"
+
+    static httpPOSTRequest(url, httpBody) {
+        console.log('post request at:', url)
+
+        let request = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        if (httpBody != null) {
+            request.body = httpBody;
+        }
+        console.log(request);
+        return fetch(this.baseURL + url, request)
+            .then(
+                response => {
+                    if (response.status == 200) {
+                        return response.json()
+                    }
+                },
+                rejected => {
+                    console.error(rejected);
+                    throw this.NETWORK_ERROR;
+                }
+            )
+    };
+
+    static httpGETRequest(url) {
+        console.log('get request at', url)
+        return fetch(this.baseURL + url, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(
+                response => {
+                    if (response.status == 200) {
+                        return response.json()
+                    } else if (response.status == 204) {
+                        return true;
+                    } else {
+                        console.log('error thrown at ', url, response);
+                        return response.json()
+                    }
+                },
+                rejected => {
+                    console.error(rejected);
+                    throw this.NETWORK_ERROR;
+                }
+            )
+    };
+
+}
